0a675472f0081429baa1b15f70193e350df00ced
Bridger Tower <bridgertower@gmail.com>
2023-09-22 11:19:16 -0600

    add og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:image" content="<generated>" />
+        <meta property="og:image:type" content="<generated>" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" />
+      </head>
       <body className="font-syne">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Cover />
