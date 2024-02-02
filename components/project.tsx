797dafb0a39e488630b905df71096f38b1bc467d
Bridger Tower <bridgertower@gmail.com>
2024-02-02 13:00:32 -0700

    update

--- components/project.tsx
+++ components/project.tsx
@@ -28,11 +28,11 @@ const Project = ({ name, link, image, desc, year }: ProjectProps) => {
           className="cursor-pointer transition-all group-hover:opacity-70"
         >
           <Link
-            className="block border rounded-lg h-56 md:h-64 relative overflow-hidden"
+            className="block border rounded-lg relative overflow-hidden"
             href={link}
           >
             <Image
-              className="w-full h-full object-cover group-hover:blur-sm object-center"
+              className="w-full h-full object-cover group-hover:blur-sm group-hover:bg-gradient-to-b from-black object-center"
               width={500}
               height={500}
               src={image}
@@ -40,15 +40,18 @@ const Project = ({ name, link, image, desc, year }: ProjectProps) => {
             ></Image>
 
             <Badge
-              variant="outline"
+              variant="secondary"
               className="hidden group-hover:flex rounded-md absolute bottom-2 left-2"
             >
               {name} <ExternalLinkIcon className="ml-2 w-3" />
             </Badge>
           </Link>
         </HoverCardTrigger>
-        <HoverCardContent>
-          {name} :: {desc}
+        <HoverCardContent
+          side="top"
+          className="text-sm fixed top-0 left-0 border shadow-lg"
+        >
+          {desc}
         </HoverCardContent>
       </HoverCard>
     </div>
